24bf95b4550f2c89a6d74206150e82b91632e7f1
Muhammad Farras Fajri <farrasfajri@gmail.com>
2025-10-26 12:23:53 +0700

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,31 @@ import cors from "cors";
 import notesRouter from "./router/notesRouter.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimit.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes/", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
